2a435625fd923182d8b2e8985f7df10ff4374e75
Adrian Tesore <adrian72112@gmail.com>
2025-05-28 22:05:56 -0300

    mejoras y unificar todo el proyecto en Laboratorio_2b

--- Laboratorio_2b/main/main.c
+++ Laboratorio_2b/main/main.c
@@ -1,13 +1,24 @@
-#include <string.h>
-#include "wifiap.h"
+#include "wifistation.h"
+#include "wifiap.h"        // ① incluir el header del AP
 #include "esp_log.h"
 
+void app_main(void) {
+    //Lo movemos aquí para que no rompa al inicializar en ambos lados
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
 
-void app_main(void)
-{
-    init_nvs();
-    ESP_LOGI(TAG, "ESP_WIFI_MODE_AP");
+    const char* ssid     = "RedTese";
+    const char* password = "Tituscan2022";
 
-    // Set to 1 for open WiFi, 0 for secured WiFi
-    wifi_init_softap(1, 1); 
+    ESP_LOGI("main", "Iniciando estación WiFi...");
+    esp_err_t ret = wifi_init_station(ssid, password);
+    if (ret == ESP_OK) {
+        ESP_LOGI("main", "WiFi conectado correctamente");
+    } else {
+        ESP_LOGE("main", "No se pudo conectar al WiFi");
+    }
+
+    // ② arranca el SoftAP sin más cambios en main
+    ESP_LOGI("main", "Iniciando punto de acceso WiFi...");
+    wifi_init_softap(/*openWifi=*/0, /*hidden=*/0);
 }
--- lab_1/build/components/parte_2_lib/CMakeFiles/parte_2_lib.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libparte_2_lib.a"
-)
