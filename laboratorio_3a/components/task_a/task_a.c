94fa0bf62ec8620461047ac72147d83755954593
agustinabacigalupe <agustina.bacigalupe@correo.ucu.edu.uy>
2025-06-12 19:10:59 -0300

    terminado

--- laboratorio_3a/components/task_a/task_a.c
+++ laboratorio_3a/components/task_a/task_a.c
@@ -1,5 +1,3 @@
-task_a:
-
 /**
  * @file task_a.c
  * @brief Tarea A: Parpadeo del LED RGB según el color actual
@@ -30,7 +28,7 @@ static led_strip_t *led_strip;
  * @param[in] pvParameters No se utiliza
  */
 static void task_a(void *pvParameters) {
-    const TickType_t delay = pdMS_TO_TICKS(500);
+    const TickType_t delay = pdMS_TO_TICKS(100);
     while (1) {
         color_t color = color_get();
 
