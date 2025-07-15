2bbad9cc6e5b159acfdf12aa0bf53597f2475dab
Sofia Nicoletti <sofia.nicoletti@correo.ucu.edu.uy>
2025-07-15 16:34:03 -0300

    modifico mttq5 app start

--- project-extra-final/main/app_main.c
+++ project-extra-final/main/app_main.c
@@ -72,8 +72,10 @@ static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_
 static void mqtt5_app_start(void)
 {
     esp_mqtt_client_config_t mqtt_cfg = {
-        .uri = MQTT_BROKER
+        .broker.address.uri = MQTT_BROKER,
+        .session.protocol_ver = MQTT_PROTOCOL_V_5,
     };
+
 
     esp_mqtt_client_handle_t client = esp_mqtt_client_init(&mqtt_cfg);
     esp_mqtt_client_register_event(client, ESP_EVENT_ANY_ID, mqtt_event_handler, NULL);
