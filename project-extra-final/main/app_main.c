edaa7cfb82d1ae5a4ac2b3b351ed33c6e659695f
Sofia Nicoletti <sofia.nicoletti@correo.ucu.edu.uy>
2025-07-15 16:55:52 -0300

    modifico mqtt client hadle y cmake

--- project-extra-final/main/app_main.c
+++ project-extra-final/main/app_main.c
@@ -77,13 +77,13 @@ static void mqtt5_app_start(void)
         .session.protocol_ver = MQTT_PROTOCOL_V_5,
     };
 
-
     esp_mqtt_client_handle_t client = esp_mqtt_client_init(&mqtt_cfg);
     esp_mqtt_client_register_event(client, ESP_EVENT_ANY_ID, mqtt_event_handler, NULL);
     esp_mqtt_client_start(client);
 
     xTaskCreate(&publish_task, "publish_task", 4096, client, 5, NULL);
 }
+
 
 void app_main(void)
 {
