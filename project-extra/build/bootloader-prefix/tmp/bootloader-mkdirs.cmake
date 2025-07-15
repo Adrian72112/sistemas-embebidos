e88a9c58443cb291fc459d09da4fdbb9f2d61e6b
Sofia Nicoletti <sofia.nicoletti@correo.ucu.edu.uy>
2025-07-15 13:34:33 -0300

    correccion de errores

---
+++ project-extra/build/bootloader-prefix/tmp/bootloader-mkdirs.cmake
@@ -0,0 +1,27 @@
+# Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+# file Copyright.txt or https://cmake.org/licensing for details.
+
+cmake_minimum_required(VERSION 3.5)
+
+# If CMAKE_DISABLE_SOURCE_CHANGES is set to true and the source directory is an
+# existing directory in our source tree, calling file(MAKE_DIRECTORY) on it
+# would cause a fatal error, even though it would be a no-op.
+if(NOT EXISTS "C:/Users/Sofia/esp/v5.4.1/esp-idf/components/bootloader/subproject")
+  file(MAKE_DIRECTORY "C:/Users/Sofia/esp/v5.4.1/esp-idf/components/bootloader/subproject")
+endif()
+file(MAKE_DIRECTORY
+  "C:/Users/Sofia/OneDrive/Documents/GitHub/sistemas-embebidos/project-extra/build/bootloader"
+  "C:/Users/Sofia/OneDrive/Documents/GitHub/sistemas-embebidos/project-extra/build/bootloader-prefix"
+  "C:/Users/Sofia/OneDrive/Documents/GitHub/sistemas-embebidos/project-extra/build/bootloader-prefix/tmp"
+  "C:/Users/Sofia/OneDrive/Documents/GitHub/sistemas-embebidos/project-extra/build/bootloader-prefix/src/bootloader-stamp"
+  "C:/Users/Sofia/OneDrive/Documents/GitHub/sistemas-embebidos/project-extra/build/bootloader-prefix/src"
+  "C:/Users/Sofia/OneDrive/Documents/GitHub/sistemas-embebidos/project-extra/build/bootloader-prefix/src/bootloader-stamp"
+)
+
+set(configSubDirs )
+foreach(subDir IN LISTS configSubDirs)
+    file(MAKE_DIRECTORY "C:/Users/Sofia/OneDrive/Documents/GitHub/sistemas-embebidos/project-extra/build/bootloader-prefix/src/bootloader-stamp/${subDir}")
+endforeach()
+if(cfgdir)
+  file(MAKE_DIRECTORY "C:/Users/Sofia/OneDrive/Documents/GitHub/sistemas-embebidos/project-extra/build/bootloader-prefix/src/bootloader-stamp${cfgdir}") # cfgdir has leading slash
+endif()
